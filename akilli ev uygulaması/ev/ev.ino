44b2000909bb1628502703791d3f11871f32393d
AlptekinBaska <148530803+AlptekinBaska@users.noreply.github.com>
2023-11-02 13:24:27 +0300

    arduino

--- akilli ev uygulaması/ev/ev.ino
+++ akilli ev uygulaması/ev/ev.ino
@@ -3,14 +3,14 @@
 #include <SoftwareSerial.h>
 double motor=6;
 double motor2=7;
-const int lm35 = A0;
+int flamePin = A0;
 double dogalgazK =8;
 int gasSensor = 9;
-float voltaj_deger = 0;
-float sicaklik = 0;
-int gelen_veri = 0;
+int flameValue = 0;
+
+
 int buzzerPin=11;
-int pir = 10;
+int pir=10;
 boolean sensorValue = 0;
 boolean gasStat   = 0;
 #include <LiquidCrystal.h>
@@ -20,13 +20,19 @@ void setup()
 {
   Serial.begin(9600);
   lcd.begin(16, 2);
-  pinMode(lm35, INPUT);
+  pinMode(flamePin,INPUT);
   pinMode(buzzerPin,OUTPUT);
   pinMode(pir, INPUT);
   pinMode(gasSensor,INPUT);
   pinMode(dogalgazK,OUTPUT);
   pinMode(motor,OUTPUT);
 pinMode(motor2,OUTPUT);
+lcd.setCursor(0,0);
+lcd.println("AKILLI EV ");
+lcd.setCursor(0,1);
+lcd.println("SISTEMI ");
+delay(500);
+lcd.clear();
 }
 
 void loop()
@@ -34,15 +40,14 @@ void loop()
   
   gasStat = digitalRead(gasSensor);
   delay(50);
-  gelen_veri = analogRead(lm35);
-   delay(50);
-  voltaj_deger = (gelen_veri / 1023.0) * 5000;
-  sicaklik = voltaj_deger / 10.0; 
+ 
  sensorValue = digitalRead(pir);
+  delay(50);
+  flameValue = digitalRead(flamePin);
   delay(100);
 
 
- if ((gasStat == 1 ) && (sensorValue == 1)&& ((sicaklik))>(50))
+ if ((gasStat == 1 ) && (sensorValue == 1)&& (flameValue == HIGH))
  {
   digitalWrite(buzzerPin, HIGH);
   delay(50);
@@ -51,13 +56,14 @@ void loop()
     delay(50);
     digitalWrite(motor,1);
     digitalWrite(motor2,0);
-    lcd.setCursor(0, 1);
+    lcd.setCursor(0, 0);
     lcd.print("ALARMMM!! ");
-    
+     lcd.setCursor(0, 1);
+     lcd.print("YANGIN GAS HIRSIZ!! ");
     delay(100);
     lcd.clear();
  }
- else if ((gasStat == 1 ) && ((sicaklik))>(50))
+ else if ((gasStat == 1 ) && (flameValue == HIGH))
  {
    digitalWrite(buzzerPin, HIGH);
     delay(100);
@@ -67,7 +73,7 @@ void loop()
     digitalWrite(motor,1);
     digitalWrite(motor2,0);
    lcd.setCursor(0, 1);
-    lcd.print("SICAKLIK GAZ!!! ");
+    lcd.print("YANGIN!!! ");
     delay(100);
     lcd.clear();
  }
@@ -77,13 +83,13 @@ else  if ((gasStat == 1 )&& (sensorValue == 1))
     delay(100);
   digitalWrite(dogalgazK, LOW);
   lcd.setCursor(0, 1);
-    lcd.print("GAZ HIRSIZ!! ");
+    lcd.print("HIRSIZ!! ");
 
     delay(100);
     lcd.clear();
     
   }
-  else if ((sensorValue == 1)&& ((sicaklik)>(50)))
+  else if ((sensorValue == 1)&& (flameValue == HIGH))
   {
      digitalWrite(buzzerPin, HIGH);
     delay(100);
@@ -92,7 +98,7 @@ else  if ((gasStat == 1 )&& (sensorValue == 1))
     digitalWrite(motor,HIGH);
     digitalWrite(motor2,LOW);
    lcd.setCursor(0, 1);
-    lcd.print("SICAKLIK HIRSIZ!");
+    lcd.print("YANGIN!");
     delay(100);
     lcd.clear();
     
@@ -101,20 +107,25 @@ else  if ((gasStat == 1 )&& (sensorValue == 1))
   {
      digitalWrite(buzzerPin, HIGH);
     delay(100);
+    lcd.setCursor(0, 0);
+    lcd.print(" ALARMM!");
+   
     lcd.setCursor(0, 1);
     lcd.print(" HIRSIZ!");
     delay(100);
     lcd.clear();
     
   }
-  else if ((sicaklik)>(50))
+  else if (flameValue == HIGH)
   {
     digitalWrite(dogalgazK,HIGH);
     delay(1);
     digitalWrite(motor,HIGH);
     digitalWrite(motor2,LOW);
+      lcd.setCursor(0, 0);
+    lcd.print("ALARM!");
     lcd.setCursor(0, 1);
-    lcd.print(" SICAKLIK!");
+    lcd.print(" YANGIN!");
   
     delay(100);
     lcd.clear();
@@ -124,6 +135,8 @@ else  if ((gasStat == 1 )&& (sensorValue == 1))
     digitalWrite(buzzerPin, HIGH);
     digitalWrite(dogalgazK,HIGH);
     delay(100);
+      lcd.setCursor(0, 0);
+     lcd.print(" ALARM!");
      lcd.setCursor(0, 1);
     lcd.print(" GAZ!");
   
@@ -139,10 +152,10 @@ else  if ((gasStat == 1 )&& (sensorValue == 1))
     lcd.clear();
     digitalWrite(motor,LOW);
     digitalWrite(motor2,LOW);
-    lcd.print("sicaklik = ");
-  lcd.setCursor(0, 1);
-  lcd.print(sicaklik);
-  lcd.print(" derece");
+    lcd.setCursor(0,0);
+    lcd.println("EV ");
+    lcd.setCursor(0,1);
+    lcd.println("GUVENLI ");
 
     delay(1000);
     lcd.clear();
